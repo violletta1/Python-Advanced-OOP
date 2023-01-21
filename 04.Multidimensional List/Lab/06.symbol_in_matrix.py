331b9018929db9b6f7d6185d31df073dfa451fce
violletta1 <villygeorgieva@yahoo.com>
2023-01-21 17:46:54 +0000

    exercise

--- 04.Multidimensional List/Lab/06.symbol_in_matrix.py
+++ 04.Multidimensional List/Lab/06.symbol_in_matrix.py
@@ -1,29 +1,55 @@
-def read_matrix_func():
-    number_of_rows = int(input())
-    current_matrix = []
+n = int(input())
 
-    for row in range(number_of_rows):
-        row_data = list(input())
-        current_matrix.append(row_data)
+matrix = []
 
-    return current_matrix
+for row in range(n):
+    matrix.append(list(input()))
+
+symbol = input()
+position = None
+for row in range(n):
+    for col in range(n):
+        if matrix[row][col] == symbol:
+            position = (row, col)
+            break
+    if position:
+        break
+
+# print(position if position else f"{symbol} does not occur in the matrix")
+
+if position:
+    print(position)
+else:
+    print(f"{symbol} does not occur in the matrix")
 
 
-def check_func_for_special_symbol(matrix, symbol):
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            current_element = matrix[row][col]
-            if current_element == symbol:
-                return row, col
-
-
-def print_func(data, symbol):
-    if data:
-        print(data)
-    else:
-        print(f'{symbol} does not occur in the matrix')
-
-
-matrix = read_matrix_func()
-special_symbol = input()
-print_func(check_func_for_special_symbol(matrix, special_symbol), special_symbol)
+#2222222222222222222
+# def read_matrix_func():
+#     number_of_rows = int(input())
+#     current_matrix = []
+#
+#     for row in range(number_of_rows):
+#         row_data = list(input())
+#         current_matrix.append(row_data)
+#
+#     return current_matrix
+#
+#
+# def check_func_for_special_symbol(matrix, symbol):
+#     for row in range(len(matrix)):
+#         for col in range(len(matrix[row])):
+#             current_element = matrix[row][col]
+#             if current_element == symbol:
+#                 return row, col
+#
+#
+# def print_func(data, symbol):
+#     if data:
+#         print(data)
+#     else:
+#         print(f'{symbol} does not occur in the matrix')
+#
+#
+# matrix = read_matrix_func()
+# special_symbol = input()
+# print_func(check_func_for_special_symbol(matrix, special_symbol), special_symbol)
